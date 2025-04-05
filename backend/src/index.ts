a44c748b8b84f7171f145061d611ce740bce660b
Shreya Sahu <shreyasahu9696@gmail.com>
2025-04-05 17:31:25 +0530

    resolve  error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,20 @@
 import { Hono } from 'hono'
-import {cors} from 'hono/cors'
-const app = new Hono() 
+import { cors } from 'hono/cors'
+
+const app = new Hono()
 
 import { userRouter } from './routes/user'
 import { blogRouter } from './routes/blog'
 
 
-app.use('/*',cors());
+app.use('/*', cors({
+  origin: 'https://blog-hive.vercel.app/', // ← Replace this with your actual frontend URL
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}))
 
-
-app.route("/api/v1/user",userRouter);
-app.route("/api/v1/blog",blogRouter);
-
-
-
-
-
-
-
+app.route("/api/v1/user", userRouter)
+app.route("/api/v1/blog", blogRouter)
 
 export default app
